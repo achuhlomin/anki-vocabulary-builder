dbc27be8600b4e3f5c8c1ba76e7a9c4f8278e205
Andrew Chuhlomin <andrewchuhlomin@gmail.com>
2020-09-22 00:40:37 +0400

    Add review of definition

--- bot.js
+++ bot.js
@@ -1,32 +1,107 @@
 import Telegraf from 'telegraf'
+import Markup from 'telegraf/markup.js'
 import {getInfo} from './src/getInfo.js'
 import {addNote} from './src/addNote.js'
 import {sync} from './src/sync.js'
 
+const dictionaryCache = {};
+const userCache = {};
+
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
-const onMessageHandler = async (ctx) => {
-  const term = ctx.message.text;
+bot.use(Telegraf.log())
 
-  await sync()
-  await ctx.replyWithChatAction('typing')
+const getInfoByTerm = async (term) => {
+  if (dictionaryCache[term]) {
+    return dictionaryCache[term]
+  }
 
   const info = await getInfo(term)
 
-  if (!info) return ctx.reply(`Word isn't found. Check availability of this one in Cambridge Dictionary`)
+  if (info && !dictionaryCache[term]) {
+    dictionaryCache[term] = info
+  }
 
-  const { error } = await addNote(info)
-
-  if (error) return ctx.reply(error)
-
-  await sync()
-
-  return ctx.reply(`"${term}" added successfully! 👍`)
+  return info
 }
 
-bot.start((ctx) => ctx.reply('Welcome! Send me a word'))
-bot.help((ctx) => ctx.reply('Send me a word that I will add to your anki'))
+const onStartHandler = (ctx) => ctx.reply('Welcome! Send me a word 🥳')
+
+const onHelpHandler = (ctx) => ctx.reply('Send me a word that I will add to your anki 🙂')
+
+const onMessageHandler = async (ctx) => {
+  try {
+    const term = ctx.message.text;
+
+    await ctx.replyWithChatAction('typing')
+
+    const info = await getInfoByTerm(term)
+
+    if (!info) return ctx.reply(`Word isn't found. Check availability of this one in Cambridge Dictionary 🤷🏼‍♀️`)
+
+    const { word, phonUK, def, part, urlUK } = info
+
+    await ctx.replyWithVoice({ filename: urlUK, url: urlUK })
+
+    const result = await ctx.reply(`${word} ${phonUK} — ${def}. ${part}`,
+      Markup.inlineKeyboard([
+        Markup.callbackButton('Add', 'add'),
+        Markup.callbackButton('Cancel', 'cancel')
+      ]).extra()
+    )
+
+    userCache[result.message_id] = term;
+  } catch (e) {
+    return ctx.reply(e.message)
+  }
+}
+
+const onAddHandler = async (ctx) => {
+  try {
+    const term = userCache[ctx.update.callback_query.message.message_id]
+    const info = dictionaryCache[term]
+
+    if (!info) {
+      await ctx.answerCbQuery()
+
+      return ctx.reply(`Failure! Add persistent cache 💩`)
+    }
+
+    const { word } = info;
+
+    await sync()
+
+    const { error } = await addNote(info)
+
+    if (error) {
+      await ctx.answerCbQuery()
+
+      return ctx.reply(`${error} 🏓`)
+    }
+
+    await sync()
+    await ctx.answerCbQuery()
+    await ctx.editMessageReplyMarkup({inline_keyboard: []})
+    await ctx.reply(`"${word}" added successfully! 👍`)
+  } catch (e) {
+    return ctx.reply(e.message)
+  }
+}
+
+const onCancelHandler = async (ctx) => {
+  try {
+    await ctx.editMessageReplyMarkup({inline_keyboard: []})
+    await ctx.answerCbQuery(`Cancelled 🌊`)
+  } catch (e) {
+    return ctx.reply(e.message)
+  }
+}
+
+bot.start(onStartHandler)
+bot.help(onHelpHandler)
 bot.on('message', onMessageHandler)
+bot.action('add', onAddHandler)
+bot.action('cancel', onCancelHandler)
 
 await bot.launch()
 
