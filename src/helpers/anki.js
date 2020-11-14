d63fe41c79c183d46bf7647d22d8b276e43e0c24
Andrew Chuhlomin <andrewchuhlomin@gmail.com>
2020-11-15 00:11:19 +0400

    Check tg id

--- src/helpers/anki.js
+++ src/helpers/anki.js
@@ -1,3 +1,8 @@
+const ids = [
+  369837507,
+  339253577,
+]
+
 const anki = {
   369837507: {
     deckName: '8. Vocabulary Builder'
@@ -7,10 +12,16 @@ const anki = {
 export const getConnect = async (ctx) => {
   const {id} = await ctx.getChat();
 
-  const customMeta = anki[id]
+  if (ids.includes(id)) {
+    const customMeta = anki[id]
 
-  return {
-    endpoint: `http://anki-${id}:8765`,
-    deckName: customMeta && customMeta.deckName ? customMeta.deckName : 'Vocabulary Builder'
+    return {
+      endpoint: `http://anki-${id}:8765`,
+      deckName: customMeta && customMeta.deckName ? customMeta.deckName : 'Vocabulary Builder'
+    }
   }
+
+  throw new Error(
+    `Failure! Your anki isn't registered 🏓`
+  )
 }
