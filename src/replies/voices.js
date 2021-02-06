289960b8e545dbe39e236ac285cf35d3bdce1ffe
Andrew Chuhlomin <andrewchuhlomin@gmail.com>
2021-02-06 22:18:53 +0400

    Throttle messages

--- src/replies/voices.js
+++ src/replies/voices.js
@@ -8,6 +8,9 @@ export const replyVoices = async (ctx, {
   const pronUrl = urlUK ? urlUK : urlUS
 
   if (pronUrl) {
+    // TODO :: Persist voices in s3
+    // TODO :: Cache s3 links
+    // TODO :: Use s3 links in anki
     const buffer = await got(pronUrl, {
       resolveBodyOnly: true,
       responseType: 'buffer',
